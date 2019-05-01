b5e71f642ae7ca8f36f283d6d229fbdff6185b0c
LeXa <lexxaa@seznam.cz>
2019-05-01 18:30:55 +0200

    Added battery management

---
+++ core/modules/battery_management/battery_management.cpp
@@ -0,0 +1,73 @@
+/*************************************************************************
+ * battery_management.cpp
+ *
+ * Created: 1.5.2019 13:31:31
+ * Revised:
+ * Author: LeXa
+ * BOARD:
+ *
+ * ABOUT:
+ *
+ *************************************************************************/
+
+#include "battery_management.h"
+
+using namespace Core::Drivers;
+ADC_DRIVER cAdc(0);
+uint16_t Core::Modules::BATTERY_MANAGEMENT::m_aVcell[];
+float Core::Modules::BATTERY_MANAGEMENT::m_fRefVoltage;
+
+void Core::Modules::BATTERY_MANAGEMENT::Init(ADC_PRESCALER_enum ePrescale, ADC_RESOLUTION_enum eResolution, ADC_REFERENCE_enum eReference, float fRefVoltage)
+{
+    cAdc.Init(ePrescale, eResolution, eReference, fRefVoltage);
+    m_fRefVoltage = fRefVoltage;
+}
+
+void Core::Modules::BATTERY_MANAGEMENT::MeasCells()
+{
+    m_aVcell[1] = cAdc.AverageConvert((ADC_MUXPOS_enum)VCELL1_ADC_MUXPOS, ADC_AVERAGE_SAMPLE_8);
+    m_aVcell[2] = cAdc.AverageConvert((ADC_MUXPOS_enum)VCELL2_ADC_MUXPOS, ADC_AVERAGE_SAMPLE_8);
+    m_aVcell[3] = cAdc.AverageConvert((ADC_MUXPOS_enum)VCELL3_ADC_MUXPOS, ADC_AVERAGE_SAMPLE_8);
+    m_aVcell[4] = cAdc.AverageConvert((ADC_MUXPOS_enum)VCELL4_ADC_MUXPOS, ADC_AVERAGE_SAMPLE_8);
+    m_aVcell[5] = cAdc.AverageConvert((ADC_MUXPOS_enum)VCELL5_ADC_MUXPOS, ADC_AVERAGE_SAMPLE_8);
+    m_aVcell[6] = cAdc.AverageConvert((ADC_MUXPOS_enum)VCELL6_ADC_MUXPOS, ADC_AVERAGE_SAMPLE_8);
+    m_aVcell[7] = cAdc.AverageConvert((ADC_MUXPOS_enum)VCELL7_ADC_MUXPOS, ADC_AVERAGE_SAMPLE_8);
+}
+
+void Core::Modules::BATTERY_MANAGEMENT::CheckCells()
+{
+    /* Disable charge */
+    for (uint8_t i=1; i<=7; i++)
+    {
+        if (GetCellVoltage(i) > 420) {vin_off(); break;}
+    }
+    
+    /* Disable discharge */
+    for (uint8_t i=1; i<=7; i++)
+    {
+        if (GetCellVoltage(i) < 300) {vout_off(); break;}
+    }
+    
+    /* Enable charge */
+    bool bEnableVin = true;
+    for (uint8_t i=1; i<=7; i++)
+    {
+        if (GetCellVoltage(i) > 410) {bEnableVin = false; break;}
+    }
+    if (bEnableVin) {vin_on();}
+    
+    /* Enable discharge */
+    bool bEnableVout = true;
+    for (uint8_t i=1; i<=7; i++)
+    {
+        if (GetCellVoltage(i) < 320) {bEnableVout = false; break;}
+    }
+    if (bEnableVout) {vout_on();}
+}
+
+uint16_t Core::Modules::BATTERY_MANAGEMENT::GetCellVoltage(uint8_t unCellNum)
+{
+    if (unCellNum == 0 || unCellNum > 7) {return 0;}
+    uint32_t unVcell = m_aVcell[unCellNum] - m_aVcell[unCellNum-1];
+    return (unVcell*3100/4096);
+}
---
+++ core/modules/battery_management/battery_management.h
@@ -0,0 +1,47 @@
+/*************************************************************************
+ * battery_management.h
+ *
+ * Created: 1.5.2019 12:11:07
+ * Revised: 
+ * Author: LeXa
+ * BOARD:
+ *
+ * ABOUT:
+ *
+ *************************************************************************/
+
+#include <core/drivers/adc.h>
+#include <conf/conf_board.h>
+
+using namespace Core::Drivers;
+
+#ifndef BATTERY_MANAGEMENT_H_
+#define BATTERY_MANAGEMENT_H_
+
+namespace Core
+{
+    namespace Modules
+    {
+        class BATTERY_MANAGEMENT
+        {
+            private:
+                static uint16_t m_aVcell[8];
+                static float m_fRefVoltage;
+                
+            public:
+                static void Init(ADC_PRESCALER_enum ePrescale, ADC_RESOLUTION_enum eResolution, ADC_REFERENCE_enum eReference, float fRefVoltage);
+                
+                static void MeasCells();
+                
+                static void CheckCells();
+                
+                static uint16_t GetCellVoltage(uint8_t unCellNum);
+            
+        };
+    }
+}
+
+
+
+
+#endif /* BATTERY_MANAGEMENT_H_ */
